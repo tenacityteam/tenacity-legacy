2a37cd19a022fd6bc63d1c0a403840eb300dfd1c
Cookie Engineer <cookiengineer@protonmail.com>
2021-07-04 10:22:57 +0200

    :shit: Remove Breakpad Crash Reports

--- src/AudacityApp.cpp
+++ src/AudacityApp.cpp
@@ -77,7 +77,6 @@ It handles initialization and termination by subclassing wxApp.
 #include "AudioIO.h"
 #include "Benchmark.h"
 #include "Clipboard.h"
-#include "CrashReport.h" // for HAS_CRASH_REPORT
 #include "commands/CommandHandler.h"
 #include "commands/AppCommandEvent.h"
 #include "widgets/ASlider.h"
@@ -122,10 +121,6 @@ It handles initialization and termination by subclassing wxApp.
 #include "ModuleManager.h"
 
 #include "import/Import.h"
-
-#if defined(USE_BREAKPAD)
-#include "BreakpadConfigurer.h"
-#endif
 
 #ifdef EXPERIMENTAL_SCOREALIGN
 #include "effects/ScoreAlignDialog.h"
@@ -382,29 +377,6 @@ void PopulatePreferences()
    gPrefs->Flush();
 }
 
-#if defined(USE_BREAKPAD)
-void InitBreakpad()
-{
-    wxFileName databasePath;
-    databasePath.SetPath(wxStandardPaths::Get().GetUserLocalDataDir());
-    databasePath.AppendDir("crashreports");
-    databasePath.Mkdir(wxS_DIR_DEFAULT, wxPATH_MKDIR_FULL);
-    
-    if(databasePath.DirExists())
-    {   
-        BreakpadConfigurer configurer;
-        configurer.SetDatabasePathUTF8(databasePath.GetPath().ToUTF8().data())
-            .SetSenderPathUTF8(wxFileName(wxStandardPaths::Get().GetExecutablePath()).GetPath().ToUTF8().data())
-    #if defined(CRASH_REPORT_URL)
-            .SetReportURL(CRASH_REPORT_URL)
-    #endif
-            .SetParameters({
-                { "version", wxString(AUDACITY_VERSION_STRING).ToUTF8().data() }
-            })
-            .Start();
-    }
-}
-#endif
 }
 
 static bool gInited = false;
@@ -952,10 +924,6 @@ wxLanguageInfo userLangs[] =
 
 void AudacityApp::OnFatalException()
 {
-#if defined(HAS_CRASH_REPORT)
-   CrashReport::Generate(wxDebugReport::Context_Exception);
-#endif
-
    exit(-1);
 }
 
@@ -1020,13 +988,10 @@ bool AudacityApp::OnExceptionInMainLoop()
 AudacityApp::AudacityApp()
 {
 #if defined(USE_BREAKPAD)
-    InitBreakpad();
 // Do not capture crashes in debug builds
 #elif !defined(_DEBUG)
-#if defined(HAS_CRASH_REPORT)
 #if defined(wxUSE_ON_FATAL_EXCEPTION) && wxUSE_ON_FATAL_EXCEPTION
    wxHandleFatalExceptions();
-#endif
 #endif
 #endif
 }
