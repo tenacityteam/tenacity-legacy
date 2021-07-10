30bcfdd36d79b1d4780b5e17f62f022d8efd2488
Emily Mabrey <emilymabrey93@gmail.com>
2021-07-10 00:08:21 -0400

    Implement CPack workaround for CMake PR #4769
    Signed-off-by: Emily Mabrey <emilymabrey93@gmail.com>

---
+++ scripts/ci/macos/hdiutil_repeat.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+((${BASH_VERSION%%.*} >= 4)) || { echo >&2 "$0: Error: Please upgrade Bash."; exit 1; }
+
+set -euxo pipefail
+
+cmd_string=/usr/bin/hdiutil "$@"
+
+max_retry=5
+
+counter=0
+
+until cmd_string
+do
+   sleep 1
+   [[ counter -eq $max_retry ]] && echo "CPack failed despite retry attempts!" && exit 1
+   echo "Trying CPack hdiutil call again. Try #$counter"
+   ((counter++))
+done
