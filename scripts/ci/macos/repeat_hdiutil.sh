8e1b90eeb92c5431fc621df0e4dc6c6eb63db7a1
Emily Mabrey <emilymabrey93@gmail.com>
2021-07-10 17:34:21 -0400

    Fix scripts/ci/macos/repeat_hdiutil.sh
    Remove -e option. Don't copy/paste without double checking closely I guess. :neutral_face:
    :heavy_dollar_sign: Add dollar signs where appropriate as reviewers pointed out.
    Replace until/do contents with easier to read if block.
    
    Signed-off-by: Emily Mabrey <emilymabrey93@gmail.com>

--- scripts/ci/macos/repeat_hdiutil.sh
+++ scripts/ci/macos/repeat_hdiutil.sh
@@ -2,18 +2,20 @@
 
 ((${BASH_VERSION%%.*} >= 4)) || { echo >&2 "$0: Error: Please upgrade Bash."; exit 1; }
 
-set -euxo pipefail
+set -uxo pipefail
 
-cmd_string=/usr/bin/hdiutil "$@" -debug
-
+cmd_string="/usr/bin/hdiutil ""$*"" -debug"
 max_retry=5
-
 counter=0
 
-until cmd_string
+until $cmd_string
 do
    sleep 1
-   [[ counter -eq $max_retry ]] && echo "CPack failed despite retry attempts!" && exit 1
-   echo "Trying CPack hdiutil call again. Try #$counter"
-   ((counter++))
+   if [[ $counter -eq $max_retry ]]; then
+        echo "CPack failed despite retry attempts!"
+        exit 1
+   else
+        echo "Trying CPack hdiutil call again. Try #$counter"
+        ((counter++))
+   fi
 done
