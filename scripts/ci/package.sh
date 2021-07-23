63e882ff9660e69d42488c8ae463278860f54d71
Emily Mabrey <emabrey@tenacityaudio.org>
2021-07-23 16:25:13 -0400

    Correct MacOS CPack configuration (#334)
    * Make the `repeat_hdiutil.sh` script more clear about retry counts.
    * Change usage of `$@` to `"$@"` to pass args correctly
    * Fix `CPACK_COMMAND_HDIUTIL` generation
    * Switch `package.sh` to use `$GITHUB_WORKSPACE`
    * Make `repeat_hdiutil.sh` executable
    * Remove `set +x` and `set -x` options
    
    Signed-off-by: Emily Mabrey <emabrey@tenacityaudio.org>

--- scripts/ci/package.sh
+++ scripts/ci/package.sh
@@ -9,8 +9,10 @@ cd build
 if [[ "${OSTYPE}" == msys* && ${GIT_BRANCH} == release* ]]; then # Windows
     cmake --build . --target innosetup --config "${AUDACITY_BUILD_TYPE}"
 else
-    export CPACK_COMMAND_HDIUTIL="./macos/repeat_hdiutil.sh"
-    cpack -C "${AUDACITY_BUILD_TYPE}" --verbose
+   # GITHUB_WORKSPACE is set by the checkout action in the action workflow configuration
+   export CPACK_COMMAND_HDIUTIL="${GITHUB_WORKSPACE}/scripts/ci/macos/repeat_hdiutil.sh"
+   chmod +x $CPACK_COMMAND_HDIUTIL
+   cpack -C "${AUDACITY_BUILD_TYPE}" -D CPACK_COMMAND_HDIUTIL="${CPACK_COMMAND_HDIUTIL}" --verbose
 fi
 
 # Remove the temporary directory
