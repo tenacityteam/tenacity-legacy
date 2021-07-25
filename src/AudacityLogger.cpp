d20a5d5b93561cf3b9e0f5896524ab7a229e366f
Panagiotis Vasilopoulos <hello@alwayslivid.com>
2021-07-25 15:09:46 +0000

    Replace Audacity reference in lastlog.txt
    Signed-off-by: Panagiotis Vasilopoulos <hello@alwayslivid.com>

--- src/AudacityLogger.cpp
+++ src/AudacityLogger.cpp
@@ -100,7 +100,7 @@ void AudacityLogger::DoLogText(const wxString & str)
 
       TimeStamp(&stamp);
 
-      mBuffer << stamp << _TS("Audacity ") << AUDACITY_VERSION_STRING << wxT("\n");
+      mBuffer << stamp << _TS("Tenacity ") << AUDACITY_VERSION_STRING << wxT("\n");
    }
 
    mBuffer << str << wxT("\n");
