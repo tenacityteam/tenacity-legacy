1f242372c97c3e6938bfb091f26c175d0e377ee7
Paul Licameli <paul.licameli@audacityteam.org>
2021-08-07 13:32:39 -0400

    Move some numbers out of ViewInfo.h ...
    ... Move them to new header TrackPanelConstants.h, to be used only by TrackPanel
    and some closely cooperating classes, but not known to the track view, controls,
    vertical ruler, or affordance classes.
    
    Also distinguish track from channel separator thickness though these remain
    equal.
    
    (cherry picked from audacity commit fba2ef009ebff6c1c43101faca6d2cb07efb83df)
    
    Signed-off-by: akleja <storspov@gmail.com>

--- src/TrackPanel.cpp
+++ src/TrackPanel.cpp
@@ -45,6 +45,7 @@ is time to refresh some aspect of the screen.
 
 
 #include "TrackPanel.h"
+#include "TrackPanelConstants.h"
 
 
 
@@ -90,6 +91,8 @@ is time to refresh some aspect of the screen.
 #include <wx/dc.h>
 #include <wx/dcclient.h>
 #include <wx/graphics.h>
+
+static_assert( kVerticalPadding == kTopMargin + kBottomMargin );
 
 /**
 
@@ -1366,7 +1369,7 @@ struct ChannelGroup final : TrackPanelGroup {
 
          auto height = view.GetHeight();
          rect.SetTop( yy );
-         rect.SetHeight( height - kSeparatorThickness );
+         rect.SetHeight( height - kChannelSeparatorThickness );
          refinement.emplace_back( yy,
             std::make_shared< VRulersAndChannels >(
                channel->shared_from_this(),
@@ -1375,7 +1378,7 @@ struct ChannelGroup final : TrackPanelGroup {
          if ( channel != pLast ) {
             yy += height;
             refinement.emplace_back(
-               yy - kSeparatorThickness,
+               yy - kChannelSeparatorThickness,
                TrackPanelResizerCell::Get( *channel ).shared_from_this() );
          }
       }
@@ -1473,7 +1476,7 @@ struct ResizingChannelGroup final : TrackPanelGroup {
    { return { Axis::Y, Refinement{
       { rect.GetTop(),
          std::make_shared< LabeledChannelGroup >( mpTrack, mLeftOffset ) },
-      { rect.GetTop() + rect.GetHeight() - kSeparatorThickness,
+      { rect.GetTop() + rect.GetHeight() - kTrackSeparatorThickness,
          TrackPanelResizerCell::Get(
             **TrackList::Channels( mpTrack.get() ).rbegin() ).shared_from_this()
       }
