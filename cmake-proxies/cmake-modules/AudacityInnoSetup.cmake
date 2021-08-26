d73e9eb1cdc5b936a4b1bec6b0c56785a75e02b9
Emily Mabrey <emabrey@tenacityaudio.org>
2021-08-26 02:54:16 -0400

    Innosetup installer updates
    Rename usage of outdated Innosetup functions
    Configure CMake to use the same build type for Innosetup target
    Use Tenacity logos
    Update readme information displayed post-install
    Update license information displayed pre-install
    Fix installer generation
    Cleanup innosetup configuration
    
    Signed-off-by: Emily Mabrey <emabrey@tenacityaudio.org>

--- cmake-proxies/cmake-modules/AudacityInnoSetup.cmake
+++ cmake-proxies/cmake-modules/AudacityInnoSetup.cmake
@@ -23,11 +23,12 @@ if( INNO_SETUP_COMPILER )
                 -DOUTPUT_DIR=${TEMP_PACKAGE_PATH}
                 -DINNO_SETUP_COMPILER=${INNO_SETUP_COMPILER}
                 -DEMBED_MANUAL=${${_OPT}package_manual}
-                -DBUILDING_64_BIT=${IS_64BIT}
+                -DIS_64_BIT=${IS_64BIT}
                 -DSIGN=${${_OPT}perform_codesign}
                 -DWINDOWS_CERTIFICATE=${WINDOWS_CERTIFICATE}
                 -D WINDOWS_CERTIFICATE_PASSWORD=${WINDOWS_CERTIFICATE_PASSWORD}
                 -P "${CMAKE_SOURCE_DIR}/win/Inno_Setup_Wizard/BuildInnoSetupInstaller.cmake"
+                -D CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
                 -parallel ${CMAKE_BUILD_PARALLEL_LEVEL}
         VERBATIM
     )
