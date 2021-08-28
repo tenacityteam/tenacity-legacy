7450432f9a480e01db79d8c9c904b165825a997c
Emily Mabrey <emabrey@tenacityaudio.org>
2021-08-28 09:21:00 -0400

    Attempt to fix macOS build issues
    Add `umount` command behavior
    Modify to retry 12 times over the course of 240 seconds total
    Change `scripts\build\macOS\DMGSetup.scpt` to close DMG when finished.
    Improve system detection for setting project name.
    
    Signed-off-by: Emily Mabrey <emabrey@tenacityaudio.org>

--- scripts/ci/macos/repeat_hdiutil.sh
+++ scripts/ci/macos/repeat_hdiutil.sh
@@ -4,11 +4,11 @@
 
 set -uo pipefail
 
-max_retry=10
+max_retry=12
 counter=0
-num_secs_await_retry=5
+num_secs_await_retry=20
 
-echo "Trying: " /usr/bin/hdiutil "$@"
+echo "Trying: /usr/bin/hdiutil $@"
 
 until /usr/bin/hdiutil "$@"; do
    sleep $num_secs_await_retry
@@ -16,7 +16,11 @@ until /usr/bin/hdiutil "$@"; do
         echo "CPack failed despite retry attempts!"
         exit 1
    else
-        echo "Trying CPack hdiutil call again. Retry attempt #$counter"
+        echo "Attempting to umount before retry..."
+        modified_args=("$@")
+        modified_args[0]="umount"
+        /usr/bin/hdiutil "${modified_args[@]}"
+        echo "Trying hdiutil call again. Retry attempt #$counter"
         ((counter++))
    fi
 done
